f8f21032e5bf085b4f87736eaee70f784dc977b8
mihai9015 <rm_popa@yahoo.com>
2018-02-26 19:14:34 +0200

    Added some translations

--- from_0/js/texts.js
+++ from_0/js/texts.js
@@ -10,5 +10,18 @@ const dict = {
   Language: {
     ro: 'Limba',
   },
+  Portfolio: {
+    ro: 'Portofoliu'
+  },
+  'about us': {
+    ro: 'Despre noi'
+  },
+  motto: {
+    en: 'get used to the best steel structures',
+    ro: 'obișnuiți-vă cu cele mai bune structuri metalice',
+  },
+  'The firm executes quality civil and industrial metal structures, of medium and small sizes.': {
+    ro: 'Firma execută structuri metalice civile și industriale, de dimensiuni medii și mici.'
+  },
 };
 window.dict = dict;
